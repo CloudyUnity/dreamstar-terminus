9e4f914fb179c4180e80449efc730e6a7b41daaa
CloudyUnity <finn.wright5@gmail.com>
2023-06-17 22:25:24 +0100

    Outline shader made for interactable objects and other stuff maybe

--- Assets/Scripts/Components/Interactable.cs
+++ Assets/Scripts/Components/Interactable.cs
@@ -7,19 +7,46 @@ using UnityEngine.Events;
 [RequireComponent(typeof(Collider2D))]
 public class Interactable : MonoBehaviour
 {
+    public static Interactable MainInteraction;
+
     PlayerInput _input;
     [SerializeField] UnityEvent<object> _event;
     [SerializeField] bool _reusable;
+    SpriteRenderer _rend;
+    M_Materials _mats;
     bool _used;
-
 
     private void Start()
     {
         _input = Singleton.Get<PlayerInput>();
+        _mats = Singleton.Get<M_Materials>();
+        _rend = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        _rend.material = MainInteraction == this ? _mats.Outline : _mats.SpriteLit;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        MainInteraction = this;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (MainInteraction == this)
+            MainInteraction = null;
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
+        if (MainInteraction == null)
+            MainInteraction = this;
+
+        if (MainInteraction != this)
+            return;
+
         if (_used && !_reusable)
             return;
 
---
+++ Assets/Scripts/Managers/Visuals and Audio/M_Materials.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M_Materials : Singleton
+{
+    public Material SpriteLit;
+    public Material SpriteUnLit;
+    public Material Outline;
+}
